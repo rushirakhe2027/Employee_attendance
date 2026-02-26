1cec701c3dbd9b4c2bc718edaa01a4eccf298db1
rushirakhe2027 <rushirakhe2027@gmail.com>
2026-02-26 15:19:35 +0530

    Optimize: Disable Flask debug mode for faster Pi 1 response

--- app.py
+++ app.py
@@ -192,4 +192,5 @@ def handle_recognition(data):
     send_webhook(data)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    # Disable debug mode for much faster performance on Pi 1
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
